59df684308b94d6dff0984815e1ec6e4d9556190
cmuty <nutipovottakvot@gmail.com>
2025-10-23 15:30:47 +0300

    Fix gradient breaking layout, make animation faster and simpler

--- Components/AnimatedGradientBackground.swift
+++ Components/AnimatedGradientBackground.swift
@@ -1,7 +1,8 @@
 import SwiftUI
 
 struct AnimatedGradientBackground: View {
-    @State private var phase: CGFloat = 0
+    @State private var startPoint: UnitPoint = .topLeading
+    @State private var endPoint: UnitPoint = .bottomTrailing
     @State private var colorIndex = 0
     
     let colorSets: [[Color]] = [
@@ -32,29 +33,23 @@ struct AnimatedGradientBackground: View {
     ]
     
     var body: some View {
-        ZStack {
-            MeshGradient(colors: currentColors)
-                .ignoresSafeArea()
-            
-            LinearGradient(
-                colors: currentColors,
-                startPoint: .topLeading,
-                endPoint: .bottomTrailing
-            )
-            .hueRotation(.degrees(phase))
-            .ignoresSafeArea()
-            .opacity(0.5)
-        }
+        LinearGradient(
+            colors: currentColors,
+            startPoint: startPoint,
+            endPoint: endPoint
+        )
+        .ignoresSafeArea()
         .onAppear {
             withAnimation(
-                .linear(duration: 8)
-                .repeatForever(autoreverses: false)
+                .easeInOut(duration: 5)
+                .repeatForever(autoreverses: true)
             ) {
-                phase = 360
+                startPoint = .bottomTrailing
+                endPoint = .topLeading
             }
             
-            Timer.scheduledTimer(withTimeInterval: 6, repeats: true) { _ in
-                withAnimation(.easeInOut(duration: 3)) {
+            Timer.scheduledTimer(withTimeInterval: 4, repeats: true) { _ in
+                withAnimation(.easeInOut(duration: 2)) {
                     colorIndex = (colorIndex + 1) % colorSets.count
                 }
             }
@@ -63,42 +58,6 @@ struct AnimatedGradientBackground: View {
     
     var currentColors: [Color] {
         colorSets[colorIndex]
-    }
-}
-
-struct MeshGradient: View {
-    let colors: [Color]
-    @State private var offset1: CGFloat = 0
-    @State private var offset2: CGFloat = 0
-    
-    var body: some View {
-        ZStack {
-            ForEach(0..<colors.count, id: \.self) { index in
-                Circle()
-                    .fill(colors[index])
-                    .frame(width: 600, height: 600)
-                    .offset(
-                        x: offset1 * CGFloat(index % 2 == 0 ? 100 : -100),
-                        y: offset2 * CGFloat(index % 3 == 0 ? 100 : -100)
-                    )
-                    .blur(radius: 100)
-            }
-        }
-        .onAppear {
-            withAnimation(
-                .easeInOut(duration: 10)
-                .repeatForever(autoreverses: true)
-            ) {
-                offset1 = 1
-            }
-            
-            withAnimation(
-                .easeInOut(duration: 12)
-                .repeatForever(autoreverses: true)
-            ) {
-                offset2 = 1
-            }
-        }
     }
 }
 
