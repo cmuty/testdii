a8d92ccf14f9c84d131c6e1bd25b65ce82ee4040
cmuty <nutipovottakvot@gmail.com>
2025-10-23 15:34:41 +0300

    Change gradient to blue, yellow, pink, purple colors with simple cycling animation

--- Components/AnimatedGradientBackground.swift
+++ Components/AnimatedGradientBackground.swift
@@ -7,28 +7,24 @@ struct AnimatedGradientBackground: View {
     
     let colorSets: [[Color]] = [
         [
-            Color(hex: "d4a5d4"),
-            Color(hex: "f0e0c0"),
-            Color(hex: "c0e0a0"),
-            Color(hex: "a0d4f0"),
-            Color(hex: "e0d0f0"),
-            Color(hex: "f0c0d4")
+            Color(hex: "6ea8ff"), // Синій
+            Color(hex: "ffd966"), // Жовтий
+            Color(hex: "ff99cc")  // Рожевий
         ],
         [
-            Color(hex: "f0c0d4"),
-            Color(hex: "c0e0f0"),
-            Color(hex: "e0f0c0"),
-            Color(hex: "d4a5d4"),
-            Color(hex: "a0d4e0"),
-            Color(hex: "f0e0c0")
+            Color(hex: "ffd966"), // Жовтий
+            Color(hex: "ff99cc"), // Рожевий
+            Color(hex: "c299ff")  // Бузковий
         ],
         [
-            Color(hex: "e0d0f0"),
-            Color(hex: "a0e0d4"),
-            Color(hex: "f0d4c0"),
-            Color(hex: "c0a0f0"),
-            Color(hex: "d4f0e0"),
-            Color(hex: "f0a0c0")
+            Color(hex: "ff99cc"), // Рожевий
+            Color(hex: "c299ff"), // Бузковий
+            Color(hex: "6ea8ff")  // Синій
+        ],
+        [
+            Color(hex: "c299ff"), // Бузковий
+            Color(hex: "6ea8ff"), // Синій
+            Color(hex: "ffd966")  // Жовтий
         ]
     ]
     
@@ -40,16 +36,8 @@ struct AnimatedGradientBackground: View {
         )
         .ignoresSafeArea()
         .onAppear {
-            withAnimation(
-                .easeInOut(duration: 5)
-                .repeatForever(autoreverses: true)
-            ) {
-                startPoint = .bottomTrailing
-                endPoint = .topLeading
-            }
-            
-            Timer.scheduledTimer(withTimeInterval: 4, repeats: true) { _ in
-                withAnimation(.easeInOut(duration: 2)) {
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+                withAnimation(.easeInOut(duration: 2.5)) {
                     colorIndex = (colorIndex + 1) % colorSets.count
                 }
             }
